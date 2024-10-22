d0f5369ed4e1c360e0efd38e93a7cc08fda302b7
Yahagi123 <164115863+Yahagi123@users.noreply.github.com>
2024-10-22 21:09:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sign Up </title>
 </head>
 <body>
-    
+    <!-- Sign IN Form  -->
+     <div class="box_container">
+            <h2>SIgn In</h2>
+
+        <form action="" method="post">
+            <div class="container">
+                <label for="Username">Usernname</label>
+            </div>
+
+
+            
+        </form>
+
+     </div>
 </body>
 </html>
