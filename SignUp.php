6a79cd3af35cea4b8ef1bc9d0d8c15c35e2a3218
Yahagi123 <164115863+Yahagi123@users.noreply.github.com>
2024-10-22 22:03:34 +0100

    added

--- SignUp.php
+++ SignUp.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Arima:wght@100..700&display=swap" rel="stylesheet">
     <title>Sign Up </title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="SignUp.css">
 </head>
 <body>
     <!-- Sign IN Form  -->
@@ -15,15 +15,23 @@
             <h2>Sign In</h2>
 
             <div class="container">
-                <label for="Username">Usernname</label>
+                <label for="Username">Username</label>
                 <input type="text" name="username" id="username" placeholder="Username" required>
+            </div>
+            <div class="container">
+                <label for="Email">Email Address</label>
+                <input type="email" name="email" id="email">
             </div>
             <div class="container">
                 <label for="Password">Password</label>
                 <input type="password" name="password" id="password" placeholder="Password" required>
             </div>
+            <div class="container">
+                <label for="Password">Confirm Password</label>
+                <input type="password" name="con_password" id="con_password" placeholder="Password" required>
+            </div>
             <div class="submit">
-            <input type="submit" value="Sign In" name="signin" id="signin">
+                <input type="submit" value="Sign In" name="signin" id="signin">
 
             </div>
             
